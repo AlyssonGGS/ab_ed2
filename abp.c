ab70d60e4311e1bb8c5b92c98931497eee7f9cbf
AlyssonGGS <alyssonggs@hotmail.com>
2016-12-05 19:13:45 -0200

    Adicionados os tipos de curriculo

--- abp.c
+++ abp.c
@@ -7,6 +7,7 @@ typedef struct curriculo
 	int cht, ntotper, tnc;
 }TCur;
 
+
 typedef struct aluno
 {
 	int mat;
@@ -19,8 +20,6 @@ typedef struct aluno
 	struct aluno *ant, *prox, *pai, *filhos;
 }TNO;
 
-
-//n sei o que fazer
 TNO *inicializa_no()
 {
 	return NULL;
@@ -36,16 +35,42 @@ TNO *cria_aluno()
 	aluno->CHCS = 0;
 	aluno->NPU = 0;
 	aluno->cur = -1;
+	aluno->folha = 0;
 	aluno->prox = aluno->ant = aluno->pai = aluno->filhos = NULL;
 	return aluno;
+}
+
+TCur *cria_curriculos()
+{
+	TCur *curs = (TCur*)malloc(sizeof(TCur)*3);
+	//curriculo 1
+	curs[0].cht = 2955;
+	curs[0].ntotper = 16;
+	curs[0].tnc = 8;
+	
+	//curriculo 2
+	curs[1].cht = 3524;
+	curs[1].ntotper = 12;
+	curs[1].tnc = 8;
+	
+	//curriculo 3
+	curs[2].cht = 3200;
+	curs[2].ntotper = 12;
+	curs[2].tnc = 8;
+	return curs;
 }
 
 
 int main (void)
 {
+	TCur *curriculos = cria_curriculos();
+	
 	int t;
 	printf("insira o valor de T:\n");
+
 	scanf("%d",&t);
 	TNO *raiz = inicializa_no();
+
+	free(curriculos);
 	return 0;
 }
